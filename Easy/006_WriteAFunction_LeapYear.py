0aa6f6dd3e045e62d33c5711288ce9ef0a0d9a2f
mspendyala <100178338+mspendyala@users.noreply.github.com>
2022-11-13 20:11:00 -0600

    Update 006_WriteAFunction_LeapYear.py

--- Easy/006_WriteAFunction_LeapYear.py
+++ Easy/006_WriteAFunction_LeapYear.py
@@ -2,15 +2,17 @@ def is_leap(year):
     leap = False
     
     # Write your logic here
-
+    
     if year % 4 == 0:
-        leap = True
-    elif year %4 != 0:
+        if year % 400 == 0:
+            leap = True
+        elif year % 100 == 0:
+            leap = False
+        else:
+            leap = True
+    elif year % 4 != 0:
         leap = False
     return leap
-
-                
-
-    
+        
 
 year = int(input())
